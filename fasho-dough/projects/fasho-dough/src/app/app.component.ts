18c6c52f3f7a964efedb802244ca7d5e352ac31c
Vedarius A. Russell <146207501+YoutacRandS-VA@users.noreply.github.com>
2024-09-27 23:46:29 +0000

    VedariusARussell

--- fasho-dough/projects/fasho-dough/src/app/app.component.ts
+++ fasho-dough/projects/fasho-dough/src/app/app.component.ts
@@ -1,13 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+@Component ({
+  imports: [
+    MatSlideToggleModule,
+  ]
 })
-export class AppComponent {
-  title = 'fasho-dough-va';
-}
+class App {fasho-dough}
